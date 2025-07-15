440cf60d9037c4b87b8657cc41b4a50eb192b7b9
Yuvi9587 <114073886+Yuvi9587@users.noreply.github.com>
2025-07-14 20:18:04 -0700

    Update MoreOptionsDialog.py

--- src/ui/dialogs/MoreOptionsDialog.py
+++ src/ui/dialogs/MoreOptionsDialog.py
@@ -22,7 +22,6 @@ class MoreOptionsDialog(QDialog):
         layout.addWidget(self.description_label)
         self.radio_button_group = QButtonGroup(self)
         self.radio_content = QRadioButton("Description/Content")
-        self.radio_comments = QRadioButton("Comments")
         self.radio_comments = QRadioButton("Comments (Not Working)")
         self.radio_button_group.addButton(self.radio_content)
         self.radio_button_group.addButton(self.radio_comments)
